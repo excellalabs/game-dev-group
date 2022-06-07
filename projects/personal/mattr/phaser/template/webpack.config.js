1df25ce453375dc0448cbd0ce37736f279af3c19
ratliff-matt-ardentmc <94627215+ratliff-matt-ardentmc@users.noreply.github.com>
2022-06-07 17:04:47 -0400

    Removed unneeded project

--- projects/personal/mattr/phaser/template/webpack.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = (env, options) => {
-  const { mode } = options;
-  return mode === 'development'
-    ? require('./config/webpack.development.config.js')(env, options)
-    : require('./config/webpack.production.config.js')(env, options);
-};
